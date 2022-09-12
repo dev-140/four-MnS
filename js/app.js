febd21de50b7fc498f9065c46e0da80b34bfeb57
rox <roxmarzan14@gmail.com>
2022-09-12 17:04:00 +0800

    updates

--- js/app.js
+++ js/app.js
@@ -1,19 +1,42 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".animate");
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 300;
-
-        if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("animate__fadeInLeft");
-        reveals[i].classList.remove("animate__fadeOutLeftBig");
-        } else {
-        reveals[i].classList.remove("animate__fadeInLeft");
-        reveals[i].classList.add("animate__fadeOutLeftBig");
+$(document).ready(function() {
+    function reveal() {
+        var reveals = document.querySelectorAll(".animate");
+    
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = reveals[i].getBoundingClientRect().top;
+            var elementVisible = 300;
+    
+            if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("animate__fadeInLeft");
+            reveals[i].classList.remove("animate__fadeOutLeftBig");
+            } else {
+            reveals[i].classList.remove("animate__fadeInLeft");
+            reveals[i].classList.add("animate__fadeOutLeftBig");
+            }
         }
     }
-}
+    
+    window.addEventListener("scroll", reveal);
 
-window.addEventListener("scroll", reveal);
+    $('.close-nav').on('click', (e)=> {
+        var $this = $(e.currentTarget);
+
+        $('.nav').toggleClass('active');
+        $('.hamburger-menu i').toggleClass('fa-xmark');
+        $('.nav-item').toggleClass('animate__bounce');
+    });
+
+    $('.nav-item').on('click', (e)=> {
+        $('.nav').removeClass('active');
+        $('.nav-item').removeClass('animate__bounce');
+    })
+
+    if ($(window).width() < 768) {
+        $('.nav-item').addClass('animate__animated');
+    }
+
+    $(window).on('resize', function() {
+        $('.nav').removeClass('active');
+    });
+});
